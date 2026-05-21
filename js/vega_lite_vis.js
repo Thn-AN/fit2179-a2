e989498e0727098aa0f59091f6915f5f0263ab61
Thien-An <tluu0037@student.monash.edu>
2026-05-22 07:33:15 +1000

    draft

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,23 @@
+var vg_1 = "js/birthplaces_map.vg.json";
+vegaEmbed("#birthplaces_map", vg_1, {"actions": false}).catch(console.error);
+
+var vg_2 = "js/careers_gantt.vg.json";
+vegaEmbed("#careers_gantt", vg_2, {"actions": false}).catch(console.error);
+
+var vg_3 = "js/world_circuits.vg.json";
+vegaEmbed("#world_circuits", vg_3, {"actions": false}).catch(console.error);
+
+var vg_4 = "js/season_2025_bump.vg.json";
+vegaEmbed("#season_2025_bump", vg_4, {"actions": false}).catch(console.error);
+
+var vg_5 = "js/aussies_per_year.vg.json";
+vegaEmbed("#aussies_per_year", vg_5, {"actions": false}).catch(console.error);
+
+var vg_6 = "js/albert_park_pole.vg.json";
+vegaEmbed("#albert_park_pole", vg_6, {"actions": false}).catch(console.error);
+
+var vg_7 = "js/aussie_season_finishes.vg.json";
+vegaEmbed("#aussie_season_finishes", vg_7, {"actions": false}).catch(console.error);
+
+var vg_8 = "js/aus_gp_grid_vs_finish.vg.json";
+vegaEmbed("#aus_gp_grid_vs_finish", vg_8, {"actions": false}).catch(console.error);
